8ce19449174db27d7eba12e605c2f8ed24abe59e
ChrisDesktop <chrisk357@gmail.com>
2019-04-28 02:39:57 -0500

    initial commit to local desktop

---
+++ MyStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
