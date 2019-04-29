539f4b0ee78bf13f40523cc41c175329b911a561
ChrisDesktop <chrisk357@gmail.com>
2019-04-28 19:56:46 -0500

    Added product.json file with dummy data

---
+++ MyStoreWeb/Data/StoreSeeder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStoreWeb.Data
+{
+    public class StoreSeeder
+    {
+        private readonly StoreContext _ctx;
+        public StoreSeeder(StoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public void Seed()
+        {
+            _ctx.Database.EnsureCreated();
+
+            if(!_ctx.Products.Any())
+            {
+                //Need to create sample data
+
+            }
+        }
+    }
+}
