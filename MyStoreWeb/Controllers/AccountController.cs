428ee1d392fa9d1631d4773287b138345574508b
ChrisDesktop <chrisk357@gmail.com>
2019-05-07 13:00:58 -0500

    Working on the Login View

---
+++ MyStoreWeb/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyStoreWeb.Data.Entities;
+
+namespace MyStoreWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
+
+        public AccountController (ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            if(this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+    }
+}
