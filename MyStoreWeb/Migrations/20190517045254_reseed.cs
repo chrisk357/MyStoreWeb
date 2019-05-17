12962cb2f06cd6baa65309c4b482b1a2e9ab0cc4
ChrisDesktop <chrisk357@gmail.com>
2019-05-17 00:33:55 -0500

    reseeded new data changed products to gear

---
+++ MyStoreWeb/Migrations/20190517045254_reseed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyStoreWeb.Migrations
+{
+    public partial class reseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
